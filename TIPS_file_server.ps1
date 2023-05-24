a79ede0491accc2ba0deea1b4e144bdb5c440220
bvtv <57758767+bvtvusn@users.noreply.github.com>
2023-05-24 22:42:36 +0200

    Added download badge

--- TIPS_file_server.ps1
+++ TIPS_file_server.ps1
@@ -315,6 +315,20 @@ $html += @"
 	</style>
 </head>
 <body>
+
+	<svg style="display: none" version="2.0">  
+	  <defs>  
+		<symbol id="download-badge">  
+		   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+			<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+		</symbol>  
+	  </defs>  
+
+	  <use href="#download-badge" />  
+	</svg> 
+	
+	
+	
 	<header class="header">
     <h1>TIPS - Powershell File Server</h1>
   </header>
@@ -355,7 +369,7 @@ $html += @"
 
 Get-ChildItem -Path $computerPath -File -Depth 0 | ForEach-Object {
     $linkpath = Join-Path -Path $curPath_Web -ChildPath $_.Name
-    $html += "<li><a href='$linkpath'>$($_.Name)</a><span class='filesize'>($(Convert-FileSize -Size $_.Length))</span></li>"
+    $html += "<li><a href='$($linkpath)?page=info'>$($_.Name)</a><div><span class='filesize'>($(Convert-FileSize -Size $_.Length))</span><a href='$linkpath'><svg width='32' height='16' viewBox='0 0 16 16' version='2.0'><use href='#download-badge' /></svg></a></div></li>"
     #Write-Host $_.Name
 }
 
